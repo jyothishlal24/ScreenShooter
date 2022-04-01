e37e2731e64d6c8f66b444b0302ce88e3019ccd3
TSC <TSC@DESKTOP-P4JT6P9>
2022-04-02 00:29:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScreenShooter
+{
+    class Program
+    {
+
+        private static int _timespan = 180000;
+        static async Task Main(string[] args)
+        {
+
+            PrintScreen ps = new PrintScreen();
+            var countScreens = 0;
+            while (true)
+            {
+                await ps.StartPrinting(ps);
+                Thread.Sleep(_timespan);
+
+                if (countScreens == 20)
+                {
+                    System.GC.Collect();
+                    countScreens = 0;
+                }
+                countScreens++;
+            }
+
+
+        }
+    }
+}
